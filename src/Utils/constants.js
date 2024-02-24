e087c3ac22107f1670256b64254bd2b420f6dab9
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-02-24 14:55:07 +0530

    fetched nowPlaying data from TMDB, made account on it.

---
+++ src/Utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODFjYzUyZmFjMzUyYWQ0NzAwZGI3MTdlNjM4OTcyZSIsInN1YiI6IjY1ZDlhYTQxZmNiOGNjMDE4NmNiMmE1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0SCSKfq4IqNdtIsZBoAK7z3FSqQPGd_NZCVFNPFFikY'
+    }
+  };
