dffb62540867f04acd76e90ce5bd55d434f2cd63
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-03-04 00:03:50 +0530

    again adding .env file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { auth } from '../Utils/firebase';
 import { signOut , onAuthStateChanged } from 'firebase/auth';
 import { useSelector, useDispatch  } from 'react-redux';
@@ -59,10 +59,8 @@ const Header = () => {
 
   return (
     <div className="absolute w-full px-[140px] py-1 bg-gradient-to-b from-black z-10 flex justify-between">
-      <Link to="/browse">
-      <img className="w-48 cursor-pointer" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
-      </Link>
-
+      <img className="w-48" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
+   
      {user && (
      <div className="flex  justify-center items-center -mr-[110px]">
 
--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -2,8 +2,8 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer' + process.env.REACT_APP_TMDB_KEY,
-    },
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODFjYzUyZmFjMzUyYWQ0NzAwZGI3MTdlNjM4OTcyZSIsInN1YiI6IjY1ZDlhYTQxZmNiOGNjMDE4NmNiMmE1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0SCSKfq4IqNdtIsZBoAK7z3FSqQPGd_NZCVFNPFFikY'
+    }
   };
 
   export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
@@ -16,4 +16,4 @@ export const API_OPTIONS = {
     { identifier: "spanish", name: "Spanish" },
   ];
 
-  export const OPENAI_KEY=process.env.REACT_APP_OPENAI_KEY;
+  export const OPENAI_KEY="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";
